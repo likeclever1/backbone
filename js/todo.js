69d4d1b9f56166e4cf8e2037398f1a01e6739d07
Yuriy Berezovskiy <yb@nxc.no>
2016-05-13 18:03:25 +0300

    update

--- js/todo.js
+++ js/todo.js
@@ -1,68 +1,50 @@
-var data = [
-    {
-        title: 'uncomplete',
-        completed: false,
-    },
-    {
-        title: 'complete',
-        completed: true,
-    },
-];
-
-var itemModel = Backbone.Model.extend();
-
-var TodosCollection = Backbone.Collection.extend({
-    model: itemModel
-});
-
-var listCollection = new TodosCollection(data);
-
-var ItemView = Backbone.View.extend({
-    events: {
-        'change input': 'toggle'
-    },
-    el: '<div class="todo__item"></div>',
-    todoTlp: Handlebars.compile($('#item-template').html()),
-
-    initialize: function() {
-        this.listenTo(this.model, "change", this.render);
-    },
-
-    render: function() {
-        this.$el.html( this.todoTlp( this.model.attributes ));
-        return this;
-    },
-
-    toggle: function(e) {
-        if($(e.target).is(":checked")) {
-            this.model.set("completed", true);
-            this.model.set("title", "completed");
-        } else {
-            this.model.set("completed", false);
-            this.model.set("title", "uncompleted");
-        }
+var TodoModel = Backbone.Model.extend({
+    defaults: {
+        title: 'title',
+        completed: true
     }
 });
 
-var ListView = Backbone.View.extend({
+var TodosCollection = Backbone.Collection.extend({
+    model: TodoModel,
+    url: "/todos",
+});
+
+var todosCollection = new TodosCollection();
+todosCollection.fetch();
+
+var TodoView = Backbone.View.extend({
     el: '#todo',
+
+    todoTpl: Handlebars.compile($('#item-template').html()),
+    events: {
+        'change': 'toggle',
+    },
 
     initialize: function() {
         this.render();
     },
 
     render: function() {
-        var me = this;
+        var data = this.collection;
+        console.log(data.size());
 
-        this.model.toJSON().forEach(function(item, index) {
-
-            var itemModelInstance = new itemModel(item);
-
-            var itemView = new ItemView({model: itemModelInstance});
-            me.$el.append(itemView.render().el);
+        var dataSorted = data.sortBy(function(item) {
+            return item.get("title").toLowerCase();
         });
+
+
+        dataSorted.forEach(function(item) {
+            console.log(item['cid']);
+            this.$el.append( this.todoTpl( item.toJSON() ) );
+        }.bind(this));
+
         return this;
+    },
+
+    toggle: function(e) {
+        console.dir(this);
     },
 });
 
-var ListView = new ListView({model: listCollection});
+var todoView = new TodoView({collection: todosCollection});
